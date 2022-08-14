55f6a511716d21cbfebb962753001384b56eef9d
OKURA Masafumi <masafumi.o1988@gmail.com>
2022-08-14 20:26:04 +0900

    Initial commit

---
+++ lib/reversible.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "reversible/version"
+
+module Reversible
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/reversible/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Reversible
+  VERSION = "0.1.0"
+end
