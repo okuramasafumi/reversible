584807a7fa99d3128f26586506cc1f74bae66a41
OKURA Masafumi <masafumi.o1988@gmail.com>
2022-08-14 21:14:16 +0900

    Feat: Implement basic functionalities

--- lib/reversible.rb
+++ lib/reversible.rb
@@ -1,8 +1,86 @@
 # frozen_string_literal: true
 
-require_relative "reversible/version"
+require_relative 'reversible/version'
 
+# Reversible module provides reversible method definition, inspired by ActiveRecord Migration
 module Reversible
   class Error < StandardError; end
-  # Your code goes here...
+
+  # @api private
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  # Used with `up` call in reversible block
+  class Up
+    # @param object [Object] a base object that every block should be evaluated on
+    def initialize(object)
+      @object = object
+    end
+
+    # `up` block in reversible block
+    def up(&block)
+      @object.instance_exec(&block)
+    end
+
+    # `down` block is noop in reversible block
+    def down(&block); end
+  end
+
+  # Used with `down` call in reversible block
+  class Down
+    # @param object [Object] a base object that every block should be evaluated on
+    def initialize(object)
+      @object = object
+    end
+
+    # `up` block is noop in reversible block
+    def up(&block); end
+
+    # `down` block in reversible block
+    def down(&block)
+      @object.instance_exec(&block)
+    end
+  end
+
+  # @api private
+  # A class that looks like a Proc but actually not, for evaluating given block in base object
+  class MethodBlock
+    def initialize(object, &block)
+      @object = object
+      @block = block
+    end
+
+    def call
+      @object.instance_exec(&@block)
+    end
+  end
+
+  # DSL
+  module ClassMethods
+    # Main method, defines two methods, `up` and `down` by default
+    #
+    # @oaram method_names [Array<Symbol, String>] an array of method names to be defined
+    # @param &block [Proc] main code called with `up` and `down` methods on base object,
+    #   in which `up` and `down` block can be used
+    # @yield [dir] Gives direction to the block, `up` or `down`
+    # @yield [dir, block] Gives direction to the block, `up` or `down`, and a block to be evaluated
+    # @return [void]
+    def reversible(*method_names, &block) # rubocop:disable Metrics/MethodLength
+      define_method(method_names.shift || :up) do |*args, **kwargs, &blk|
+        if blk
+          instance_exec(Up.new(self), MethodBlock.new(self, &blk), *args, **kwargs, &block)
+        else
+          instance_exec(Up.new(self), *args, **kwargs, &block)
+        end
+      end
+      define_method(method_names.shift || :down) do |*args, **kwargs, &blk|
+        if blk
+          instance_exec(Down.new(self), MethodBlock.new(self, &blk), *args, **kwargs, &block)
+        else
+          instance_exec(Down.new(self), *args, **kwargs, &block)
+        end
+      end
+    end
+  end
 end
--- test/test_reversible.rb
+++ test/test_reversible.rb
@@ -1,13 +1,88 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class TestReversible < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Reversible::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  class Foo
+    include Reversible
+
+    def initialize(id)
+      @id = id
+    end
+
+    reversible do |dir|
+      puts 'Before action'
+      dir.up { puts "Up with id: #{@id}" }
+      dir.down { puts "Down with id: #{@id}" }
+      puts 'After action'
+    end
+  end
+
+  def test_it_works_with_instance_variable
+    assert_output("Before action\nUp with id: 1\nAfter action\nBefore action\nDown with id: 1\nAfter action\n") do
+      foo = Foo.new(1)
+      foo.up
+      foo.down
+    end
+  end
+
+  class Bar
+    include Reversible
+
+    def initialize(data)
+      @data = data
+    end
+
+    reversible :update, :undo_update do |dir, new_data|
+      puts "Before: #{@data}"
+      dir.up do
+        @old_data = @data.dup
+        @data.update(new_data)
+      end
+      dir.down { @data = @old_data.dup }
+      puts "After: #{@data}"
+    end
+  end
+
+  def test_it_works_with_argument
+    assert_output("Before: {:id=>1}\nAfter: {:id=>2}\nBefore: {:id=>2}\nAfter: {:id=>1}\n") do
+      bar = Bar.new(id: 1)
+      bar.update(id: 2)
+      bar.undo_update
+    end
+  end
+
+  class Baz
+    include Reversible
+
+    attr_accessor :count
+
+    def initialize
+      @count = 0
+    end
+
+    reversible do |dir, block|
+      dir.up { puts 'up!' }
+      dir.down { puts 'down!' }
+      block.call
+    end
+  end
+
+  def test_it_works_with_block # rubocop:disable Metrics/MethodLength
+    assert_output("up!\n1\ndown!\n0\n") do
+      baz = Baz.new
+      baz.up do
+        self.count += 1
+        puts self.count
+      end
+      baz.down do
+        self.count -= 1
+        puts self.count
+      end
+    end
   end
 end
